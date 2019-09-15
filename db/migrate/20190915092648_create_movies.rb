db432fd545dff17867813a7cf8a0cd5812439df3
michaelstsnow <michael.st.snow@gmail.com>
2019-09-15 09:28:46 +0000

    Automatically backed up by Learn

--- db/migrate/20190915092648_create_movies.rb
+++ db/migrate/20190915092648_create_movies.rb
@@ -1,12 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    create_table :publications do |t|
+    create_table :movies do |t|
       t.string :title
-      t.text :description
-      t.references :publication_type
-      t.integer :publisher_id
-      t.string :publisher_type
-      t.boolean :single_issue
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
 
       t.timestamps
     end
